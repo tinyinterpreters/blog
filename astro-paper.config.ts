4b8e0f182ac3a1f38f28cb899014f47ab9f6dd9b
Dwayne Crooks <dwayne@tinyinterpreters.dev>
2026-07-01 06:48:05 -0400

    Configure astro-paper.config.ts

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -2,19 +2,19 @@ import { defineAstroPaperConfig } from "./src/types/config";
 
 export default defineAstroPaperConfig({
   site: {
-    url: "https://astro-paper.pages.dev/",
-    title: "AstroPaper",
-    description: "A minimal, responsive and SEO-friendly Astro blog theme.",
-    author: "Sat Naing",
-    profile: "https://satna.ing",
+    url: "https://blog.tinyinterpreters.dev/",
+    title: "Tiny Interpreters",
+    description: "Hand-crafted, byte-sized interpreters with Elm.",
+    author: "Dwayne Crooks",
+    profile: "https://elmwithdwayne.dev/",
     ogImage: "default-og.jpg",
     lang: "en",
-    timezone: "Asia/Bangkok",
+    timezone: "America/Port_of_Spain",
     dir: "ltr",
   },
   posts: {
-    perPage: 4,
-    perIndex: 4,
+    perPage: 20,
+    perIndex: 5,
     scheduledPostMargin: 15 * 60 * 1000,
   },
   features: {
@@ -24,15 +24,15 @@ export default defineAstroPaperConfig({
     showBackButton: true,
     editPost: {
       enabled: true,
-      url: "https://github.com/satnaing/astro-paper/edit/main/",
+      url: "https://github.com/tinyinterpreters/blog/edit/master/",
     },
     search: "pagefind",
   },
   socials: [
-    { name: "github",   url: "https://github.com/satnaing/astro-paper" },
-    { name: "x",        url: "https://x.com/username" },
-    { name: "linkedin", url: "https://www.linkedin.com/in/username/" },
-    { name: "mail",     url: "mailto:yourmail@gmail.com" },
+    { name: "github",   url: "https://github.com/dwayne" },
+    // { name: "x",        url: "https://x.com/username" },
+    // { name: "linkedin", url: "https://www.linkedin.com/in/username/" },
+    { name: "mail",     url: "mailto:dwayne@tinyinterpreters.dev" },
   ],
   shareLinks: [
     { name: "whatsapp", url: "https://wa.me/?text=" },
