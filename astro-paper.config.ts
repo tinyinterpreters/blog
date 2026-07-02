a0edf25662626549b77fe3f6ba4e89df0b5912ec
Dwayne Crooks <dwayne@tinyinterpreters.dev>
2026-07-02 05:31:08 -0400

    Change the GitHub URL

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -29,7 +29,7 @@ export default defineAstroPaperConfig({
     search: "pagefind",
   },
   socials: [
-    { name: "github",   url: "https://github.com/dwayne" },
+    { name: "github",   url: "https://github.com/tinyinterpreters" },
     // { name: "x",        url: "https://x.com/username" },
     // { name: "linkedin", url: "https://www.linkedin.com/in/username/" },
     { name: "mail",     url: "mailto:dwayne@tinyinterpreters.dev" },
