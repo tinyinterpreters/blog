8bbdb1f5fb9f2bae2373f5512e1ddf4fc2417c0d
Dwayne Crooks <dwayne@tinyinterpreters.dev>
2026-09-21 14:43:12 -0400

    Update devalue

--- nix/blog.nix
+++ nix/blog.nix
@@ -37,7 +37,7 @@ stdenv.mkDerivation (finalAttrs: {
     inherit (finalAttrs) pname version src;
     inherit pnpm;
     fetcherVersion = 4;
-    hash = "sha256-w2okl+E0HrqQyaYmC7WiTEl/lmzNKjRolm063fS28pY=";
+    hash = "sha256-5YTcSSzuRV10U5MAtsnpTyV6jGmUvCC5SCI72bBwDT8=";
   };
 
   buildPhase = ''
